9528cf26390ee2590952b0d8663ad26a1195c459
UDamindu <daminduuthpala@gmail.com>
2016-09-17 15:37:13 +0530

    initial commit

---
+++ DemoInstitute/src/com/demoinstitute/DemoInstitute.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.demoinstitute;
+
+/**
+ *
+ * @author Arunalu Hasakelum
+ */
+public class DemoInstitute {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
