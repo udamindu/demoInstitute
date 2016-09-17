d0bf5e64542ff12fad8cc620add5acda36444abd
UDamindu <daminduuthpala@gmail.com>
2016-09-17 16:46:08 +0530

    Student class done

--- DemoInstitute/src/com/demoinstitute/entity/Student.java
+++ DemoInstitute/src/com/demoinstitute/entity/Student.java
@@ -6,8 +6,7 @@ package com.demoinstitute.entity;
 
 import java.util.HashSet;
 import java.util.Set;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  *
@@ -20,4 +19,33 @@ public class Student extends Person{
     
     private String registrationId;
     private Set<Course> listOfCourses = new HashSet<>();
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    //@ElementCollection(fetch = FetchType.EAGER)
+    @JoinTable(name = "STUDENT_ENROLLED_COURSES_TAB", 
+            joinColumns = {@JoinColumn(name = "STUDENT_ID")}, 
+            inverseJoinColumns = {@JoinColumn(name = "COURSE_ID")})
+    public Set<Course> getListOfCourses() {
+        return listOfCourses;
+    }
+
+    public void setListOfCourses(Set<Course> listOfCourses) {
+        this.listOfCourses = listOfCourses;
+    }
+
+    @Column(name = "REGISTRATION_ID")
+    public String getRegistrationId() {
+        return registrationId;
+    }
+
+    public void setRegistrationId(String registrationId) {
+        this.registrationId = registrationId;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nRegistration ID : " + getRegistrationId() +
+                "\nCourses : " + getListOfCourses().toString();
+    }
 }
